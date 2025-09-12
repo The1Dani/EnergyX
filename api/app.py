bc669953683781c0d9a366e2776733cbd732422e
Canter Daniel <83081029+The1Dani@users.noreply.github.com>
2025-09-12 22:49:21 +0300

    added data as json and added api endpoint

--- api/app.py
+++ api/app.py
@@ -1,9 +1,20 @@
 from flask import Flask
-
+import read_data
 
 app = Flask(__name__)
 
+import json
+data = {}
+# Opening JSON file
+with open('../data/data.json') as json_file:
+    data:dict = json.load(json_file)
 
-@app.route("/")
-def hello():
-    return {"hello": "world"}
+keys = list(data.keys())
+
+@app.route("/id/<id>")
+def hello(id):
+    return data[str(id)]
+
+@app.route("/keys")
+def keys_route():
+    return keys
--- api/read_data.py
+++ api/read_data.py
@@ -9,12 +9,25 @@ def read_one_file(file:str) -> dict:
     
 
 def read_all_files() -> list[dict]:
-    return read_one_file(f"../data/{filename.get_all_file_names()[0]}")
+    # return read_one_file(f"../data/{filename.get_all_file_names()[0]}")
     return [read_one_file(f"../data/{f}") for f in filename.get_all_file_names()]
 
+def merge(to: dict, d2:dict):
+    for key, value in d2.items():
+        if key in to:
+            to[key] += value  # concatenate lists
+        else:
+            to[key] = value
+
+def merge_all(dicts: list[dict]) -> dict:
+    merged = {}
+    for d in dicts:
+        merge(merged, d)
+    return merged
 
 def main():
-    print(read_all_files())
+    ds = read_all_files()
+    return merge_all(ds)
 
 if __name__ == "__main__" :
     main()
